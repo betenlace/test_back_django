3fc7755743216484ba101b482a90d3a8b6da90f8
cesaraph <capimient@utp.edu.co>
2021-06-18 22:23:00 -0500

    testing

--- interview/settings.py
+++ interview/settings.py
@@ -75,13 +75,13 @@ WSGI_APPLICATION = 'interview.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+if "DATABASE_URL" not in os.environ:
+    os.environ[
+        "DATABASE_URL"
+    ] = " "
 
+DATABASES = {}
+DATABASES["default"] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
